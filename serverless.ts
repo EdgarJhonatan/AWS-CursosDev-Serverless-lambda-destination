2d7a096ad16cffa7168a424b1f596d8cad204d13
Edgar Delgadillo <edgar.delgadillo@acceso.com.pe>
2021-10-10 19:14:58 -0500

    primer commit

---
+++ serverless.ts
@@ -0,0 +1,32 @@
+import type { AWS } from "@serverless/typescript";
+
+import destination from "@functions/destination";
+
+const serverlessConfiguration: AWS = {
+  service: "lambdadestination",
+  frameworkVersion: "2",
+  custom: {
+    webpack: {
+      webpackConfig: "./webpack.config.js",
+      includeModules: true,
+    },
+  },
+  plugins: ["serverless-webpack"],
+  provider: {
+    name: "aws",
+    runtime: "nodejs14.x",
+    region: "us-east-2",
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+    },
+    lambdaHashingVersion: "20201221",
+  },
+  // import the function via paths
+  functions: { destination },
+};
+
+module.exports = serverlessConfiguration;
